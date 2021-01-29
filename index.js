e55792af4af8eb5cc613a70887d5e63b88386dba
Jerry092383 <jerry092383@mail.chungyo.net>
2021-01-29 14:21:20 +0800

    modify command code

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const token = '1682168127:AAHIJDoxpAj2yZc23EJ-HZ3oc2SA1FLhVhA';
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\start/, (msg) => {
+bot.onText(/\/start/, (msg) => {
+    console.log('msg :>> ', msg);
     const chatId = msg.chat.id;
     const res = `${msg.chat.username} 你好`;
     bot.sendMessage(chatId, res);
