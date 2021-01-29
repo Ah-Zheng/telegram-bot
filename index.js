b83b5523dc520bdceaa5d8e7d9f34a5ea5c6c825
Jerry092383 <jerry092383@mail.chungyo.net>
2021-01-29 13:55:48 +0800

    init my bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1682168127:AAHIJDoxpAj2yZc23EJ-HZ3oc2SA1FLhVhA';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\start/, (msg) => {
+    const chatId = msg.chat.id;
+    const res = `${msg.chat.username} 你好`;
+    bot.sendMessage(chatId, res);
+});
