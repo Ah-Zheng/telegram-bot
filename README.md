c3099abd99db369f151380929436d04a343b84dd
Jerry092383 <jerry092383@mail.chungyo.net>
2021-02-01 15:26:31 +0800

    test inline button and send html message

---
+++ README.md
@@ -0,0 +1,7 @@
+# 阿正 Telegram Bot
+
+## Command
+
+start - say hello
+test - test
+chatInfo - get chat info
--- index.js
+++ index.js
@@ -3,9 +3,51 @@ const token = '1682168127:AAHIJDoxpAj2yZc23EJ-HZ3oc2SA1FLhVhA';
 
 const bot = new TelegramBot(token, { polling: true });
 
+// 587290206 me - bot
+
 bot.onText(/\/start/, (msg) => {
-    console.log('msg :>> ', msg);
     const chatId = msg.chat.id;
     const res = `${msg.from.username} 你好`;
+    bot.sendMessage(msg, res);
+    bot.sendMessage(chatId, `${msg.from.first_name} 傳訊息`, {
+        reply_markup: {
+            one_time_keyboard: true,
+            keyboard: [['我要訂餐']]
+        }
+    });
+    bot.sendMessage(chatId, 'ttttttt', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: '阿正萬事屋', url: 'https://ah-zheng.com'}]
+            ]
+        }
+    });
+});
+
+bot.onText(/\/test/, (msg) => {
+    const chatId = msg.chat.id;
+    const res = 'test';
     bot.sendMessage(chatId, res);
+});
+
+bot.onText(/\/chatinfo/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `
+        <code>
+            id: ${msg.chat.id}
+            title: ${msg.chat.title}
+            name: ${msg.chat.first_name}
+        </code>
+    `, {
+        parse_mode: 'html'
+    });
+});
+
+bot.on('message', (msg) => {
+    if (msg.text.toString() === '我要訂餐') {
+        bot.sendMessage(msg.chat.id, '請問要吃啥？')
+    }
+    if (msg.text.toString() === 'location') {
+        bot.sendLocation(msg.chat.id, 24.164013322287058, 120.63768283651885)
+    }
 });
